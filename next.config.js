74d549abafd57ca9661fe857a94c65efceee4e58
Chrisdocs <chriswolfe.dev@gmail.com>
2022-06-17 11:34:05 -0500

    removed next config export

--- next.config.js
+++ next.config.js
@@ -4,19 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-module.exports = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    });
-    return config;
-  }
-}
